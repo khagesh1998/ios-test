faa26277dce4f3c6d3a776120f95b3429eafda51
khagesh1998 <khagesh.kumar@dream11.com>
2021-11-13 14:40:09 +0530

    Initial Commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Khagesh on 13/11/21.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
